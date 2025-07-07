a703b4f7fe92d4a41059152d197cf83e95753e5b
D. Sohan Kumar <sohans1092004@gmail.com>
2025-07-07 13:00:07 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/about', (req, res) => {
+  res.send('About Page')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`)
+})
